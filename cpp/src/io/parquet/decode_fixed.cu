f924dac6409a905e6c83d88a8a1d4f5420f701ab
Paul Mattione <pmattione@nvidia.com>
2025-09-02 18:23:26 -0400

    only fill in null zeroes for string columns

--- cpp/src/io/parquet/decode_fixed.cu
+++ cpp/src/io/parquet/decode_fixed.cu
@@ -1257,22 +1257,22 @@ CUDF_KERNEL void __launch_bounds__(decode_block_size_t, 8)
     valid_count = next_valid_count;
   }
 
-  // Zero-fill null positions after decoding valid values
-  if (should_process_nulls) {
-    uint32_t const dtype_len = has_strings_t ? sizeof(cudf::size_type) : s->dtype_len;
-    int const num_values     = [&]() {
-      if constexpr (has_lists_t) {
-        auto const& ni = s->nesting_info[s->col.max_nesting_depth - 1];
-        return ni.valid_map_offset - init_valid_map_offset;
-      } else {
-        return s->num_rows;
-      }
-    }();
-    zero_fill_null_positions_shared<decode_block_size_t>(
-      s, dtype_len, init_valid_map_offset, num_values, t);
-  }
-
   if constexpr (has_strings_t) {
+    // Zero-fill null positions after decoding valid values
+    if (should_process_nulls) {
+      uint32_t const dtype_len = has_strings_t ? sizeof(cudf::size_type) : s->dtype_len;
+      int const num_values     = [&]() {
+        if constexpr (has_lists_t) {
+          auto const& ni = s->nesting_info[s->col.max_nesting_depth - 1];
+          return ni.valid_map_offset - init_valid_map_offset;
+        } else {
+          return s->num_rows;
+        }
+      }();
+      zero_fill_null_positions_shared<decode_block_size_t>(
+        s, dtype_len, init_valid_map_offset, num_values, t);
+    }
+
     // For large strings, update the initial string buffer offset to be used during large string
     // column construction. Otherwise, convert string sizes to final offsets.
     if (s->col.is_large_string_col) {
